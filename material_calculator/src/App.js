4e1b49f6eb813238af474b5acc554b5c0cae67fd
Civoremo <omerovic_n@hotmail.com>
2019-12-24 15:15:21 -0500

    fixed annoying shallow copy error

--- material_calculator/src/App.js
+++ material_calculator/src/App.js
@@ -10,18 +10,16 @@ import Results1x2 from "./components/results1by2";
 class App extends Component {
 	state = {
 		pieces1x2: [3, 3, 5, 8, 11, 14, 14],
+		// pieces1x2: [],
 		pieces2x2: [],
 		material1x2: [20, 24, 30],
 		material2x2: [],
-		used1x2: 0,
-		used2x2: 0,
-		waste: 0,
 		cut1x2text: "",
 		cut2x2text: "",
 		length1x2materialText: "",
 		length2x2materialText: "",
-		result1x2: "",
-		result2x2: "",
+		result1x2: [],
+		result2x2: [],
 		result1x2CutList: [],
 		result2x2CutList: [],
 	};
@@ -36,7 +34,6 @@ class App extends Component {
 	add1x2Cut = event => {
 		event.preventDefault();
 		this.setState({
-			// pieces1x2: this.state.pieces1x2.push(parseInt(this.state.cut1x2text)),
 			pieces1x2: [...this.state.pieces1x2, parseInt(this.state.cut1x2text)],
 		});
 	};
@@ -63,89 +60,138 @@ class App extends Component {
 		});
 	};
 
-	result1x2Posting = (size, waste, quantity, cuttingList) => {
+	result1x2Posting = (result, cuttingList) => {
+		console.log("MESSAGE FOR THIS MATERIAL", cuttingList);
 		this.setState({
-			result1x2: `Size of: ${size}in   ||   Quantity: ${quantity}   ||   Waste: ${waste}in`,
-			result1x2CutList: cuttingList,
+			result1x2: [...this.state.result1x2, ...result],
+			result1x2CutList: [...this.state.result1x2CutList, cuttingList],
 		});
+		console.log("UPDATED MESSAGE ", this.state.result1x2CutList);
 	};
 
 	calculate1x2 = event => {
+		event.preventDefault();
 		let waste = 0;
-		let cutsArray = this.state.pieces1x2;
-		let materialSize_1 = 0;
+		let cutsArray = [...this.state.pieces1x2];
+		let materialCount = 0;
 		let order = [];
 		let cutList = [];
+		let currentMaterial = 0;
+		let resultMessage = [];
 
-		// for (let z = 0; z < this.state.material1x2.length - 1; z++) {}
-		while (cutsArray.length > 0) {
-			let currentMaterial = this.state.material1x2[0];
-			materialSize_1++;
-			// console.log("--- Cuts done on this material: ", order);
-			cutList.push({ currentMaterial: order });
+		for (let y = 0; y < this.state.material1x2.length; y++) {
+			cutList.push([]);
+			resultMessage.push([]);
+		}
+		console.log("NEW Cutlist ", cutList);
+
+		for (let z = 0; z < this.state.material1x2.length; z++) {
+			waste = 0;
+			cutsArray = [...this.state.pieces1x2];
+			materialCount = 0;
 			order = [];
-			// console.log("NEW material started; count: ", materialSize_1);
-			while (currentMaterial > 0 && cutsArray.length > 0) {
-				if (cutsArray[cutsArray.length - 1] > currentMaterial) {
-					// console.log("");
-					// console.log("TOO SHORT");
-					for (let i = cutsArray.length - 1; i > -1; i--) {
-						// console.log("ENTRY ARR 1: ", cutsArray);
-						// console.log("index ", i);
-						if (cutsArray[i] <= currentMaterial) {
-							// console.log("matrial length: ", currentMaterial);
-							// console.log("found: ", cutsArray[i], " at index ", i);
-							currentMaterial -= cutsArray[i];
-							// console.log("material after cutting: ", currentMaterial);
-							// console.log("removed item 1: ", cutsArray[i]);
-							order.push(cutsArray[i]);
-							cutsArray = cutsArray.slice(0, i).concat(cutsArray.slice(i + 1, cutsArray.length));
-							// this.removeItem(cutsArray, i);
-							// console.log("array after slice: ", cutsArray);
-						}
-					}
+			currentMaterial = this.state.material1x2[z];
 
+			console.log("");
+			console.log("-------------------------");
+			console.log("NEW MATERIAL SIZE START");
+			console.log("length", this.state.material1x2.length, " z is ", z);
+			console.log("NEW Cutlist ", cutList);
+			console.log("Outcome ", this.state.result1x2);
+			console.log("ORIGINAL ARR ", this.state.pieces1x2);
+			console.log(
+				"Number of Material Needed: ",
+				materialCount,
+				" | Pieces to Cut Needed: ",
+				cutsArray,
+				" | Wasted Material:  ",
+				waste,
+				" | Material Size: ",
+				this.state.material1x2[z]
+			);
+			console.log("");
+			console.log("-------------------------");
+			console.log("");
+
+			while (cutsArray.length > 0) {
+				console.log("*********    LOOKING AT ARRAY    **************** ", this.state.pieces1x2);
+				currentMaterial = this.state.material1x2[z];
+				materialCount++;
+				console.log("--- Cuts made on this material: ", order);
+				cutList[z].push({ currentMaterial: order });
+				order = [];
+				console.log("NEW material started; count: ", materialCount);
+				while (currentMaterial > 0 && cutsArray.length > 0) {
+					if (cutsArray[cutsArray.length - 1] > currentMaterial) {
+						console.log("");
+						console.log("TOO SHORT");
+						for (let i = cutsArray.length - 1; i >= 0; i--) {
+							console.log("ENTRY ARR 1: ", cutsArray);
+							console.log("index ", i);
+							if (cutsArray[i] <= currentMaterial) {
+								console.log("matrial length: ", currentMaterial);
+								console.log("found: ", cutsArray[i], " at index ", i);
+								currentMaterial -= cutsArray[i];
+								console.log("material after cutting: ", currentMaterial);
+								console.log("removed item 1: ", cutsArray[i]);
+								order.push(cutsArray[i]);
+								cutsArray = cutsArray.slice(0, i).concat(cutsArray.slice(i + 1, cutsArray.length));
+								// this.removeItem(cutsArray, i);
+								console.log("array after slice: ", cutsArray);
+							}
+						}
+
+						waste += currentMaterial;
+						console.log("WASTE 1: ", waste);
+						console.log("Material is to small for any of our cuts");
+						break;
+					} else {
+						console.log("");
+						console.log("BIG ENOUGH");
+						console.log("ENTRY ARR 2: ", cutsArray);
+						console.log("material length: ", currentMaterial);
+						currentMaterial -= cutsArray[cutsArray.length - 1];
+						console.log("material after cutting: ", currentMaterial);
+						console.log("removed item 2: ", cutsArray[cutsArray.length - 1]);
+						order.push(cutsArray[cutsArray.length - 1]);
+						cutsArray.pop();
+						console.log("array after we cut: ", cutsArray);
+					}
+				}
+				if (cutsArray.length === 0) {
 					waste += currentMaterial;
-					// console.log("WASTE 1: ", waste);
-					// console.log("Material is to small for any of our cuts");
-					break;
-				} else {
-					// console.log("");
-					// console.log("BIG ENOUGH");
-					// console.log("ENTRY ARR 2: ", cutsArray);
-					// console.log("material length: ", currentMaterial);
-					currentMaterial -= cutsArray[cutsArray.length - 1];
-					// console.log("material after cutting: ", currentMaterial);
-					// console.log("removed item 2: ", cutsArray[cutsArray.length - 1]);
-					order.push(cutsArray[cutsArray.length - 1]);
-					cutsArray.pop();
-					// console.log("array after we cut: ", cutsArray);
+					// console.log("WASTE 2: ", waste);
 				}
 			}
-			if (cutsArray.length === 0) {
-				waste += currentMaterial;
-				// console.log("WASTE 2: ", waste);
-			}
+			console.log("");
+			console.log("");
+			console.log(`Material Length ${this.state.material1x2[z]} COMPLETE`);
+			console.log("PIECES ARR ", this.state.pieces1x2);
+			console.log("");
+			console.log("--- Cuts made on this material: ", order);
+			cutList[z].push({ currentMaterial: order });
+			cutList[z].shift();
+			console.log("LIST FOR CUTTING ", cutList);
+			console.log(
+				"pieces needed ",
+				materialCount,
+				` size: ${this.state.material1x2[z]}`,
+				" waste created: ",
+				waste,
+				" remaining cuts: ",
+				cutsArray
+			);
+			resultMessage[z].push(
+				`Size of: ${this.state.material1x2[z]}in   ||   Quantity: ${materialCount}   ||   Waste: ${waste}in`
+			);
+			this.result1x2Posting(resultMessage, cutList);
 		}
-		// console.log("--- Cuts done on this material: ", order);
-		cutList.push({ currentMaterial: order });
-		cutList.shift();
-		// console.log("LIST FOR CUTTING ", cutList);
-		// console.log(
-		// 	"pieces needed ",
-		// 	materialSize_1,
-		// 	` size: ${this.state.material1x2[0]}`,
-		// 	" waste created: ",
-		// 	waste,
-		// 	" remaining cuts: ",
-		// 	cutsArray
-		// );
-		this.result1x2Posting(this.state.material1x2[0], waste, materialSize_1, cutList);
 	};
 
 	render() {
 		return (
 			<div className="App">
+				{console.log("OUR ORIGINAL ARR ", this.state.pieces1x2)}
 				<div
 					style={{
 						display: "flex",
--- material_calculator/src/components/results1by2.js
+++ material_calculator/src/components/results1by2.js
@@ -3,13 +3,14 @@ import React from "react";
 const Results1x2 = props => {
 	const { result1x2, result1x2CutList } = props;
 	console.log("RESULTS ", result1x2CutList);
+	console.log("MESSAGE ", result1x2);
 
 	if (result1x2CutList === "undefined" || result1x2CutList.length === 0) {
 		return <div style={{ marginTop: "10px" }}>No Results Yet</div>;
 	}
 	return (
 		<div>
-			<div>{result1x2}</div>
+			{/* <div>{result1x2[0]}</div>
 			{result1x2CutList.map((obj, index) => {
 				console.log("OBJECT array ", obj);
 				console.log("each item ", obj.currentMaterial);
@@ -22,7 +23,7 @@ const Results1x2 = props => {
 						]
 					</div>
 				);
-			})}
+			})} */}
 		</div>
 	);
 };
